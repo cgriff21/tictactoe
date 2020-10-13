b8732e52c11cdb9f6bd26f921d9c502c3803e5e4
chano212 <chance@thechanceproject.org>
2020-10-12 20:43:21 -0700

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React Parcel Micro App!</h1>
+             <p>yerrrr.</p>
+    </>
+  );
+};
+
+export default App;
+
