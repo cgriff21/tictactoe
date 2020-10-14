2035f41fceb435f36e369c01c7653a68cc2d55ab
chano212 <chance@thechanceproject.org>
2020-10-13 20:52:49 -0700

    create status message component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { useState } from 'react';
 import Board from './components/Board';
+import History from './components/History';
+import StatusMessage from './components/StatusMessage';
+
 import { calculateWinner } from './helpers';
 
 import './styles/root.scss';
 
 const App = () => {
-  // const [board, setBoard] = useState(Array(9).fill(null));
-  // const [isXNext, setIsXNext] = useState(false);
-
   const [history, setHistory] = useState([
     { board: Array(9).fill(null), isXNext: true },
   ]);
   const [currentMove, setCurrentMove] = useState(0);
-
   const current = history[currentMove];
 
   const winner = calculateWinner(current.board);
-
-  const message = winner
-    ? `Winner is ${winner}`
-    : `Next player is ${current.isXNext ? 'X' : 'O'}`;
 
   const handleSquareClick = position => {
     if (current.board[position] || winner) {
@@ -42,11 +37,16 @@ const App = () => {
     setCurrentMove(prev => prev + 1);
   };
 
+  const moveTo = move => {
+    setCurrentMove(move);
+  };
+
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <h2>{message}</h2>
+      <StatusMessage winner={winner} current={current} />
       <Board board={current.board} handleSquareClick={handleSquareClick} />
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
 };
